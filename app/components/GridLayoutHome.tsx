6e168cbc9333b3be45c3676139423f960f572752
jmmndza <jmmndz3312@gmail.com>
2026-03-19 08:47:09 +0800

    light theme

--- app/components/GridLayoutHome.tsx
+++ app/components/GridLayoutHome.tsx
@@ -5,17 +5,17 @@ export function GridLayoutHome() {
     const [isSignUp, setIsSignUp] = useState(false);
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] px-4 w-full mb-5 md:h-[60vh]">
+        <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] px-4 w-full mb-5 lg:h-[60vh]">
             {/* NexusFlow HomePage Hero */}
             <div className="">
-                <div className="flex flex-col justify-center items-start flex-wrap p-10 text-4xl font-extralight gap-3 h-full">
+                <div className="flex flex-col justify-center flex-wrap p-5 md:text-2xl lg:text-4xl font-extralight gap-3 h-full">
                     <h1>NexusFlow Headline Message.</h1>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium</p>
                 </div>
             </div>
 
             {/* Auth Card */}
-            <div className="bg-base-100 rounded-[var(--radius-box)] m-4 md:m-10 shadow-lg flex items-center justify-center md:h-full">
+        <div className="bg-base-100 rounded-[var(--radius-box)] m-4 lg:m-10 shadow-lg flex items-center justify-center lg:h-full">
                 <div className="flex flex-col justify-center items-center text-center gap-5 p-6 w-full">
                     {isSignUp ? (
                         <>
