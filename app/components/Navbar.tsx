8665b198534581fcda3ce87e49c2b1a312634ed3
JmMNDZA <jmmndz3312@gmail.com>
2026-03-19 08:43:11 +0800

    responsive changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export function Navbar({modifyType}: {modifyType?: string}) {
     if (modifyType === "simple") {
     return (
-        <div className="navbar mb-5">
+        <div className="navbar mb-2">
             <div className="flex justify-center flex-1">
                 <a className="btn btn-ghost normal-case text-xl">NexusFlow</a>
             </div>
